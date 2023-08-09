8fbf3d2ff3c6d37843db7a8603c4be30e59d2be2
chatgpt9 <128497084+chatgpt9@users.noreply.github.com>
2023-08-09 16:14:14 +0330

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const puppeteer = require('puppeteer');
 const cookie = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiMDkzMzUwNDYzNTIiLCJpc3MiOiJhdXRoIiwidmVyaWZpZWRfdGltZSI6MTY5MTU4MDg2MywiaWF0IjoxNjkxNTgwODYzLCJleHAiOjE2OTI4NzY4NjMsInVzZXItdHlwZSI6InBlcnNvbmFsIiwidXNlci10eXBlLWZhIjoiXHUwNjdlXHUwNjQ2XHUwNjQ0IFx1MDYzNFx1MDYyZVx1MDYzNVx1MDZjYyIsInNpZCI6ImM3NmU5ODIxLWZkMGQtNDM3YS1hOGJiLTRlNzE5YTI5Y2E2ZiJ9.y2CertlwVCfiM-dSTMAAo4XRO5uERCJxsQeDfdVA31M';
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: 'new' // Use the new headless mode
+  });
+
   const page = await browser.newPage();
 
   // Set the cookie
@@ -17,7 +20,7 @@ const cookie = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiMDkzMzUwNDYzNTI
   await page.goto('https://divar.ir/services/craftsmen/AhSkX');
 
   // Click the button
-  const buttonSelector = 'button[class*="your-button-class"]';
+  const buttonSelector = 'button[class="kt-button kt-button--primary"]';
   await page.waitForSelector(buttonSelector);
   await page.click(buttonSelector);
 
