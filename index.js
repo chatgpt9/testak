fad24a4b4973ba2453d6038c83e6511acfa558e4
chatgpt9 <128497084+chatgpt9@users.noreply.github.com>
2023-08-09 15:42:19 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,30 @@
 const axios = require('axios');
-const { JSDOM } = require('jsdom');
+const cheerio = require('cheerio');
 
-const tokenValue = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiMDkzMzUwNDYzNTIiLCJpc3MiOiJhdXRoIiwidmVyaWZpZWRfdGltZSI6MTY5MTU4MDg2MywiaWF0IjoxNjkxNTgwODYzLCJleHAiOjE2OTI4NzY4NjMsInVzZXItdHlwZSI6InBlcnNvbmFsIiwidXNlci10eXBlLWZhIjoiXHUwNjdlXHUwNjQ2XHUwNjQ0IFx1MDYzNFx1MDYyZVx1MDYzNVx1MDZjYyIsInNpZCI6ImM3NmU5ODIxLWZkMGQtNDM3YS1hOGJiLTRlNzE5YTI5Y2E2ZiJ9.y2CertlwVCfiM-dSTMAAo4XRO5uERCJxsQeDfdVA31M';
+const cookie = 'token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiMDkzMzUwNDYzNTIiLCJpc3MiOiJhdXRoIiwidmVyaWZpZWRfdGltZSI6MTY5MTU4MDg2MywiaWF0IjoxNjkxNTgwODYzLCJleHAiOjE2OTI4NzY4NjMsInVzZXItdHlwZSI6InBlcnNvbmFsIiwidXNlci10eXBlLWZhIjoiXHUwNjdlXHUwNjQ2XHUwNjQ0IFx1MDYzNFx1MDYyZVx1MDYzNVx1MDZjYyIsInNpZCI6ImM3NmU5ODIxLWZkMGQtNDM3YS1hOGJiLTRlNzE5YTI5Y2E2ZiJ9.y2CertlwVCfiM-dSTMAAo4XRO5uERCJxsQeDfdVA31M';
 
-// Set the request headers including the "token" cookie
-const headers = {
-  'Cookie': `token=${tokenValue}`,
-};
-
-// URL to fetch content from
 const url = 'https://divar.ir/services/craftsmen/AhSkX';
+const buttonXpath = '//*[@id="app"]/div[2]/div/div/div[1]/div[2]/button[1]';
 
-// Make the HTTP GET request
-axios.get(url, { headers })
-  .then(response => {
-    const html = response.data;
-
-    // Use JSDOM to parse the HTML and interact with it
-    const dom = new JSDOM(html);
-    const { document } = dom.window;
-
-    // Find and click the button using XPath
-    const buttonXPath = '//*[@id="app"]/div[2]/div/div/div[1]/div[2]/button[1]';
-    const button = document.evaluate(buttonXPath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-
-    if (button) {
-      button.click();
-
-      // Display the response text after clicking the button
-      console.log(dom.window.document.body.textContent);
-    } else {
-      console.log('Button not found');
-    }
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+axios.get(url, {
+  headers: {
+    Cookie: cookie
+  }
+})
+.then(response => {
+  const html = response.data;
+  const $ = cheerio.load(html);
+  
+  const button = $(buttonXpath);
+  
+  if (button.length === 0) {
+    console.log('Button not found.');
+    return;
+  }
+  
+  const buttonContent = button.text().trim();
+  console.log('Button Content:', buttonContent);
+})
+.catch(error => {
+  console.error('Error:', error.message);
+});
