0c1d1958fd92f0c49efa16b549001e6ea9e24ea7
chatgpt9 <128497084+chatgpt9@users.noreply.github.com>
2023-08-09 15:38:26 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,38 @@
 const axios = require('axios');
-const cheerio = require('cheerio');
+const { JSDOM } = require('jsdom');
 
-// Set your token value here
-const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiMDkzMzUwNDYzNTIiLCJpc3MiOiJhdXRoIiwidmVyaWZpZWRfdGltZSI6MTY5MTU4MDg2MywiaWF0IjoxNjkxNTgwODYzLCJleHAiOjE2OTI4NzY4NjMsInVzZXItdHlwZSI6InBlcnNvbmFsIiwidXNlci10eXBlLWZhIjoiXHUwNjdlXHUwNjQ2XHUwNjQ0IFx1MDYzNFx1MDYyZVx1MDYzNVx1MDZjYyIsInNpZCI6ImM3NmU5ODIxLWZkMGQtNDM3YS1hOGJiLTRlNzE5YTI5Y2E2ZiJ9.y2CertlwVCfiM-dSTMAAo4XRO5uERCJxsQeDfdVA31M';
+const tokenValue = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiMDkzMzUwNDYzNTIiLCJpc3MiOiJhdXRoIiwidmVyaWZpZWRfdGltZSI6MTY5MTU4MDg2MywiaWF0IjoxNjkxNTgwODYzLCJleHAiOjE2OTI4NzY4NjMsInVzZXItdHlwZSI6InBlcnNvbmFsIiwidXNlci10eXBlLWZhIjoiXHUwNjdlXHUwNjQ2XHUwNjQ0IFx1MDYzNFx1MDYyZVx1MDYzNVx1MDZjYyIsInNpZCI6ImM3NmU5ODIxLWZkMGQtNDM3YS1hOGJiLTRlNzE5YTI5Y2E2ZiJ9.y2CertlwVCfiM-dSTMAAo4XRO5uERCJxsQeDfdVA31M';
 
-// Set the URL
+// Set the request headers including the "token" cookie
+const headers = {
+  'Cookie': `token=${tokenValue}`,
+};
+
+// URL to fetch content from
 const url = 'https://divar.ir/services/craftsmen/AhSkX';
 
-// Set the CSS selector for the button
-const buttonSelector = '.post-actions__get-contact.kt-button--primary.kt-button';
+// Make the HTTP GET request
+axios.get(url, { headers })
+  .then(response => {
+    const html = response.data;
 
-async function main() {
-  try {
-    // Make a GET request with the token in the cookie header
-    const response = await axios.get(url, {
-      headers: {
-        Cookie: `token=${token}`,
-      },
-    });
+    // Use JSDOM to parse the HTML and interact with it
+    const dom = new JSDOM(html);
+    const { document } = dom.window;
 
-    // Load the HTML content into cheerio for parsing
-    const $ = cheerio.load(response.data);
+    // Find and click the button using XPath
+    const buttonXPath = '//*[@id="app"]/div[2]/div/div/div[1]/div[2]/button[1]';
+    const button = document.evaluate(buttonXPath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 
-    // Find the button and extract its URL
-    const buttonUrl = $(buttonSelector).attr('href');
+    if (button) {
+      button.click();
 
-    if (buttonUrl) {
-      // Make a GET request to the button URL
-      const contactResponse = await axios.get(buttonUrl);
-
-      // Print the response data as text
-      console.log(contactResponse.data);
+      // Display the response text after clicking the button
+      console.log(dom.window.document.body.textContent);
     } else {
-      console.log('Button not found.');
+      console.log('Button not found');
     }
-  } catch (error) {
-    console.error('Error:', error.message);
-  }
-}
-
-main();
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
