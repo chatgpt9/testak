6f369241bb6a53dec51914415b1c6a55c61141bd
chatgpt9 <128497084+chatgpt9@users.noreply.github.com>
2023-08-09 16:10:44 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,68 +1,36 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
+const puppeteer = require('puppeteer');
 
-const cookie = 'token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiMDkzMzUwNDYzNTIiLCJpc3MiOiJhdXRoIiwidmVyaWZpZWRfdGltZSI6MTY5MTU4MDg2MywiaWF0IjoxNjkxNTgwODYzLCJleHAiOjE2OTI4NzY4NjMsInVzZXItdHlwZSI6InBlcnNvbmFsIiwidXNlci10eXBlLWZhIjoiXHUwNjdlXHUwNjQ2XHUwNjQ0IFx1MDYzNFx1MDYyZVx1MDYzNVx1MDZjYyIsInNpZCI6ImM3NmU5ODIxLWZkMGQtNDM3YS1hOGJiLTRlNzE5YTI5Y2E2ZiJ9.y2CertlwVCfiM-dSTMAAo4XRO5uERCJxsQeDfdVA31M';
+const cookie = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiMDkzMzUwNDYzNTIiLCJpc3MiOiJhdXRoIiwidmVyaWZpZWRfdGltZSI6MTY5MTU4MDg2MywiaWF0IjoxNjkxNTgwODYzLCJleHAiOjE2OTI4NzY4NjMsInVzZXItdHlwZSI6InBlcnNvbmFsIiwidXNlci10eXBlLWZhIjoiXHUwNjdlXHUwNjQ2XHUwNjQ0IFx1MDYzNFx1MDYyZVx1MDYzNVx1MDZjYyIsInNpZCI6ImM3NmU5ODIxLWZkMGQtNDM3YS1hOGJiLTRlNzE5YTI5Y2E2ZiJ9.y2CertlwVCfiM-dSTMAAo4XRO5uERCJxsQeDfdVA31M';
 
-const url = 'https://divar.ir/services/craftsmen/AhSkX';
-const buttonClass = 'kt-button kt-button--primary'; // Replace with the actual class of the button
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-axios.get(url, {
-  headers: {
-    Cookie: cookie
-  }
-})
-.then(response => {
-  const html = response.data;
-  const $ = cheerio.load(html);
-
-  const button = $(`button[class*="${buttonClass}"]`);
-
-  if (button.length === 0) {
-    console.log('Button not found.');
-    return;
-  }
-
-  // Extract the JavaScript code from the onclick attribute
-  const onclickAttr = button.attr('onclick');
-  if (!onclickAttr) {
-    console.log('Button does not have an onclick attribute.');
-    return;
-  }
-
-  // Extract the URL from the JavaScript code
-  const match = onclickAttr.match(/window\.location\.href\s*=\s*'([^']+)'/);
-  if (!match) {
-    console.log('Could not extract URL from onclick attribute.');
-    return;
-  }
-
-  const clickUrl = match[1];
-
-  // Make an additional HTTP request to the extracted URL
-  return axios.get(clickUrl, {
-    headers: {
-      Cookie: cookie
-    }
+  // Set the cookie
+  await page.setCookie({
+    name: 'token',
+    value: cookie,
+    domain: 'divar.ir'
   });
-})
-.then(response => {
-  if (!response) {
-    return;
-  }
 
-  const clickResponse = response.data;
-  const $ = cheerio.load(clickResponse);
+  // Navigate to the URL
+  await page.goto('https://divar.ir/services/craftsmen/AhSkX');
 
-  const aTag = $('a.kt-unexpandable-row__action.kt-text-truncate');
+  // Click the button
+  const buttonSelector = 'button[class*="your-button-class"]';
+  await page.waitForSelector(buttonSelector);
+  await page.click(buttonSelector);
 
-  if (aTag.length === 0) {
-    console.log('Anchor tag not found.');
-    return;
-  }
+  // Wait for 5 seconds
+  await page.waitForTimeout(5000);
 
-  const hrefValue = aTag.attr('href');
+  // Retrieve the href value
+  const hrefValue = await page.evaluate(() => {
+    const anchor = document.querySelector('a.kt-unexpandable-row__action.kt-text-truncate');
+    return anchor ? anchor.href : null;
+  });
+
   console.log('Href Value:', hrefValue);
-})
-.catch(error => {
-  console.error('Error:', error.message);
-});
+
+  await browser.close();
+})();
