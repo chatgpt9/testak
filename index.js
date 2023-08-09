71239092c56ab4eb990ff8d7c0be8ca669dfe62a
chatgpt9 <128497084+chatgpt9@users.noreply.github.com>
2023-08-09 15:58:01 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,43 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
-const puppeteer = require('puppeteer'); // Add puppeteer for headless browser automation
 
 const cookie = 'token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiMDkzMzUwNDYzNTIiLCJpc3MiOiJhdXRoIiwidmVyaWZpZWRfdGltZSI6MTY5MTU4MDg2MywiaWF0IjoxNjkxNTgwODYzLCJleHAiOjE2OTI4NzY4NjMsInVzZXItdHlwZSI6InBlcnNvbmFsIiwidXNlci10eXBlLWZhIjoiXHUwNjdlXHUwNjQ2XHUwNjQ0IFx1MDYzNFx1MDYyZVx1MDYzNVx1MDZjYyIsInNpZCI6ImM3NmU5ODIxLWZkMGQtNDM3YS1hOGJiLTRlNzE5YTI5Y2E2ZiJ9.y2CertlwVCfiM-dSTMAAo4XRO5uERCJxsQeDfdVA31M';
 
 const url = 'https://divar.ir/services/craftsmen/AhSkX';
-const buttonClass = 'your-button-class'; // Replace with the actual class name of the button
-const linkClass = 'kt-unexpandable-row__action'; // Class of the link to retrieve
 
-(async () => {
-  try {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-
-    await page.setExtraHTTPHeaders({
-      'Cookie': cookie
-    });
-
-    await page.goto(url);
-    
-    // Wait for the button to be available
-    await page.waitForSelector(`button.${buttonClass}`);
-
-    // Click the button
-    await page.click(`button.${buttonClass}`);
-    
-    // Wait for 5 seconds
-    await page.waitForTimeout(5000);
-
-    // Get the href value of the specified link
-    const linkHref = await page.evaluate(linkClass => {
-      const linkElement = document.querySelector(`a.${linkClass}`);
-      return linkElement ? linkElement.getAttribute('href') : null;
-    }, linkClass);
-
-    if (linkHref) {
-      console.log('Link Href:', linkHref);
-    } else {
-      console.log('Link not found.');
-    }
-
-    await browser.close();
-  } catch (error) {
-    console.error('Error:', error);
+axios.get(url, {
+  headers: {
+    Cookie: cookie
   }
-})();
+})
+.then(response => {
+  const html = response.data;
+  const $ = cheerio.load(html);
+  
+  const button = $('button[class*="your-button-class"]');
+  
+  if (button.length === 0) {
+    console.log('Button not found.');
+    return;
+  }
+  
+  const buttonContent = button.text().trim();
+  console.log('Button Content:', buttonContent);
+  
+  button.click(); // Simulate click action
+  
+  setTimeout(() => {
+    const aTag = $('a.kt-unexpandable-row__action.kt-text-truncate');
+    
+    if (aTag.length === 0) {
+      console.log('Anchor tag not found.');
+      return;
+    }
+    
+    const hrefValue = aTag.attr('href');
+    console.log('Href Value:', hrefValue);
+  }, 5000); // Wait for 5 seconds before retrieving href value
+})
+.catch(error => {
+  console.error('Error:', error.message);
+});
